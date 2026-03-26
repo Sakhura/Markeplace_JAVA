97710789ac61a6c579c1c87f7068b1d082328bd9
Sakhura <sabina.romero@gmail.com>
2026-03-25 21:01:29 -0300

    modelo ok

--- src/main/java/model/Producto.java
+++ src/main/java/model/Producto.java
@@ -1,4 +1,46 @@
 package model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3 , max =120)
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "La descripcion es obligatorio")
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @NotBlank(message = "El precio es obligatorio")
+    @DecimalMin(value="0.01", message="El precio debe ser mayor a Cero")
+    @Column(nullable = false)
+    private Double precio;
+
+    @NotBlank(message = "La categoria es obligatoria")
+    private String categoria;
+
+    private String imagenUrl;
+
+    @Column(nullable = false)
+    private boolean activo = true;
+
 }
