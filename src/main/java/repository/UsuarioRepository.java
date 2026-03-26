dab9bd682a1ff401aa9f113d1cfc79569d1831cf
Sakhura <sabina.romero@gmail.com>
2026-03-25 22:35:20 -0300

    carpeta repository

--- src/main/java/repository/UsuarioRepository.java
+++ src/main/java/repository/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
-public class UsuarioRepository {
+import model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByEmail(String email);
 }
