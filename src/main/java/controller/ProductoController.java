eca9c58f366a09416db4ec8191cdf79d9e1fa493
Sakhura <sabina.romero@gmail.com>
2026-03-25 22:09:53 -0300

    producto Controller

--- src/main/java/controller/ProductoController.java
+++ src/main/java/controller/ProductoController.java
@@ -1,4 +1,58 @@
 package controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import model.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import service.ProductoService;
+
+@Controller
+@RequestMapping("/productos")
+@RequiredArgsConstructor
 public class ProductoController {
+
+    private final ProductoService productoService;
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAllAttributes("productos", productoService.listarTodos());
+        return "productos/lista";
+    }
+
+    @GetMapping("/nuevo")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String guardar(@Valid @ModelAttribute Producto producto,
+                          BindingResult result,
+                          RedirectAttributes flash){
+        if(result.hasErrors()){
+            return "productos/formulario";
+        }
+        productoService.guardar(producto);
+        flash.addFlashAttribute("message", "Producto publicado correctamente");
+        return  "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String editar(@PathVariable Long id, Model model){
+        model.addAllAttributes("producto", productoService.buscarPorId(id));
+        return "productos/formulario";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String eliminar(@PathVariable Long id, RedirectAttributes flash){
+        productoService.eliminar(id);
+        flash.addFlashAttribute("message", "Producto eliminado correctamente");
+        return "redirect:/";
+    }
 }
